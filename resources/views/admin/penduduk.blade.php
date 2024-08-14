f2670aae0f2e46675636f1190928e9b9f6245064
Muhammad Rikzan <30933800+rikzanx@users.noreply.github.com>
2024-08-14 13:59:32 +0700

    diabel reponsive

--- resources/views/admin/penduduk.blade.php
+++ resources/views/admin/penduduk.blade.php
@@ -591,7 +591,6 @@
       var table = $('#example1').DataTable({
           orderCellsTop: true,
           fixedHeader: false,
-          responsive: true,  // Menambahkan opsi responsif
           "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"],
           initComplete: function () {
               var api = this.api();
